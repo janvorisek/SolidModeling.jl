e32816af7a8387cb9500e6f32b3b8c4fb6c591ac
Jan Vorisek <jan@vorisek.me>
2020-02-16 19:26:57 +0100

    test: add missing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,6 @@ function cubeUnion()
     return volume(c)
 end
 
-
 function cubeUnionReversed()
     c1 = cube(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
     c2 = cube(0.5, 0.5, 0.5, 2.0, 2.0, 2.0);
@@ -19,9 +18,33 @@ function cubeUnionReversed()
     return volume(c)
 end
 
+# Test subtraction of two cubes
+function cubeSubtract()
+    c1 = cube(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+    c2 = cube(0.5, 0.5, 0.5, 2.0, 2.0, 2.0)
+    r = CSG.subtract(c1, c2)
+
+    v = volume(r)
+
+    return v
+end
+
+# Test intersection of two cubes
+function cubeIntersect()
+    c1 = cube(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+    c2 = cube(0.5, 0.5, 0.5, 2.0, 2.0, 2.0)
+
+    v = volume(CSG.intersect(c1, c2))
+
+    return v
+end
+
+
 @testset begin
     @test isapprox(cubeUnion(), 4.25)                   # Test against known volume
     @test isapprox(cubeUnion(), cubeUnionReversed())    # Test if ordering doesnt matter
+    @test isapprox(cubeSubtract(), (1-0.125))
+    @test isapprox(cubeIntersect(), 0.125)
 end
 
 
